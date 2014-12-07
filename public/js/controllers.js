7ac75d779623040ef754c92d36e9bb5fc99131dc
David Rosson <david@rosson.com.au>
2014-12-08 02:22:17 +1100

    8080 no more

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,10 +3,10 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
 
 	$scope.updateQuery= function() {
-        $http.get('http://finn-rosson.rhcloud.com:8080/sana/' + $scope.query).success(function(data) {
+        $http.get('http://finn-rosson.rhcloud.com/sana/' + $scope.query).success(function(data) {
 			$scope.sanat = data;
   			});
-  		$http.get('http://finn-rosson.rhcloud.com:8080/full/' + $scope.query).success(function(data) {
+  		$http.get('http://finn-rosson.rhcloud.com/full/' + $scope.query).success(function(data) {
     		$scope.quotes = data;
   			});
     };
