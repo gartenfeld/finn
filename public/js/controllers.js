0359a6dfa29b84b55506f0411be5b34f80e6b99d
David Rosson <david@rosson.com.au>
2014-12-22 16:09:35 +1100

    fixed empty query

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,12 +3,15 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
 
 	$scope.updateQuery= function() {
-        $http.get('http://finn-rosson.rhcloud.com/sana/' + $scope.query).success(function(data) {
-			$scope.sanat = data;
-  			});
-  		$http.get('http://finn-rosson.rhcloud.com/full/' + $scope.query).success(function(data) {
-    		$scope.quotes = data;
-  			});
+
+		if ($scope.query != "") {
+	        $http.get('http://finn-rosson.rhcloud.com/sana/' + $scope.query).success(function(data) {
+				$scope.sanat = data;
+	  			});
+	  		$http.get('http://finn-rosson.rhcloud.com/full/' + $scope.query).success(function(data) {
+	    		$scope.quotes = data;
+	  			});
+  		};
     };
 
 }]);
