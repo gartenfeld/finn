ecb8e657f0acf488ff429fd91b436f4c5f7efcf0
David Rosson <david@rosson.com.au>
2015-08-30 13:31:50 -0700

    updated file structure

--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ var app = express();
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.set('port', server_port); 
-app.set('ipaddr', server_ip);
+app.set('port', port); 
+app.set('ipaddr', ip);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/sana/:query', api.findWord); 
 app.get('/full/:query', api.findText); 
 
-app.listen(server_port, server_ip);
+app.listen(port, ip);
