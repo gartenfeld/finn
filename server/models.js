4ba073afaad46bd2f77b56646e331164e2ac6db7
David Rosson <david@rosson.com.au>
2015-10-06 06:48:43 -0700

    added server side cache

--- server/models.js
+++ server/models.js
@@ -6,35 +6,50 @@ var dbUser = process.env.SUOMI_USERNAME,
     dbPort = 43037,
     uri = 'mongodb://' + dbHost + ':' + dbPort + '/suomi';
 
+
+var wordCache = {},
+    textCache = {};
+
 var models = {};
 
-models.getHeadword = function (searchString, callback) {
-  mongo.connect(uri, function (err, db) {
-    db.collection("sanat").find(
-      { $text: { $search: searchString.toString() } },  
-      { limit : 10, sort : "headword" }, 
-      function (err, cursor) {
-        cursor.toArray(function (err, docs) {
-          callback(docs);
-        });
-      });
-  });
-};
-
-models.getCitations = function (searchText, callback) {
-  mongo.connect(uri, function (err, db) {
-    db.collection("citations", function (err, collection) {
-      collection.find(
-        { $text: { $search: searchText.toString() } },  
-        { limit : 10 }, 
+models.getHeadword = function (query, callback) {
+  query = query.toString();
+  if (wordCache[query]) {
+    callback(wordCache[query]);
+  } else {
+    mongo.connect(uri, function (err, db) {
+      db.collection("sanat").find(
+        { $text: { $search: query } },  
+        { limit : 10, sort : "headword" }, 
         function (err, cursor) {
-          cursor.toArray(
-            function (err, docs) {
-              callback(docs);
-            });
+          cursor.toArray(function (err, docs) {
+            wordCache[query] = docs;
+            callback(docs);
+            db.close();
+          });
         });
     });
-  });
+  }
+};
+
+models.getCitations = function (query, callback) {
+  query = query.toString();
+  if (textCache[query]) {
+    callback(textCache[query]);
+  } else {
+    mongo.connect(uri, function (err, db) {
+      db.collection("citations").find(
+        { $text: { $search: query } },  
+        { limit : 10 }, 
+        function (err, cursor) {
+          cursor.toArray(function (err, docs) {
+            textCache[query] = docs;
+            callback(docs);
+            db.close();
+          });
+        });
+    });
+  }
 };
 
 module.exports = models;
