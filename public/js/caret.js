eb90e1daa7448be5ac83edb9ab3cd54ffe02bee3
David Rosson <david@rosson.com.au>
2016-11-09 20:33:47 -0800

    Prevent double-caret

--- public/js/caret.js
+++ public/js/caret.js
@@ -1,16 +1,20 @@
-var $caret = document.getElementById('caret');
-var $input = document.getElementById('search');
-
-$caret.blink = function() {
-  $caret.style.opacity = 0.75;
-  var delay = setTimeout(function() {
-    $caret.style.opacity = 0;
-    window.clearTimeout(delay);
-  }, 400);
-};
-
-var timer = setInterval(function() {
-  if ($input.value === '') {
-    $caret.blink();
-  }
-}, 2500);
+(function() {
+  var $caret = document.getElementById('caret');
+  var $input = document.getElementById('search');
+  var isMobile = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  var hasFocus = true;
+  $input.onfocus = function() { hasFocus = true; };
+  $input.onblur = function() { hasFocus = false; };
+  $caret.blink = function() {
+    $caret.style.opacity = 0.75;
+    var delay = setTimeout(function() {
+      $caret.style.opacity = 0;
+      window.clearTimeout(delay);
+    }, 400);
+  };
+  var timer = setInterval(function() {
+    if ($input.value === '' && (!isMobile || !hasFocus)) {
+      $caret.blink();
+    }
+  }, 2500);
+})();
