1c92417e9db11009d7b22100c80cf95d0b5f33d0
David Rosson <david@rosson.com.au>
2016-11-09 21:23:16 -0800

    Display results for only the last query

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,16 +2,24 @@ angular.module('finn', [])
 	.controller('appController', ['$scope', '$http', function ($scope, $http) {
 		var HOST = 'http://finn-rosson.rhcloud.com/';
 		var DEBOUNCE_WAIT = 200;
+		var latestQueryId;
+		function _setResults(queryId, prop, data) {
+			if (queryId === latestQueryId) {
+				$scope[prop] = data;
+			}
+		}
 		function _fetchData() {
 			var query = $scope.query.trim();
 			if (!query) { return; }
+			var queryId = Number(new Date());
+			latestQueryId = queryId;
 			$http.get(HOST + 'sana/' + query)
 				.success(function(data) {
-					$scope.sanat = data;
+					_setResults(queryId, 'sanat', data);
 				});
 			$http.get(HOST + 'full/' + query)
 				.success(function(data) {
-				  $scope.quotes = data;
+				  _setResults(queryId, 'quotes', data);
 				});
 			window.ga('send', 'event', 'Query', 'Entered', query);
 		}
