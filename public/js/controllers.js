7249ade869222c5eb1c80f12147a9b5638e1574b
David Rosson <david@rosson.com.au>
2016-11-22 21:52:27 -0800

    Lint indentation

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,31 +1,31 @@
 angular.module('finn', [])
-	.controller('appController', ['$scope', '$http', function ($scope, $http) {
-		var HOST = 'http://finn-rosson.rhcloud.com/';
-		var DEBOUNCE_WAIT = 250;
+  .controller('appController', ['$scope', '$http', function ($scope, $http) {
+    var HOST = 'http://finn-rosson.rhcloud.com/';
+    var DEBOUNCE_WAIT = 250;
     var logQuery = window.debounce(function _logQuery(query) {
       window.ga('send', 'event', 'Query', 'Entered', query);
     }, DEBOUNCE_WAIT * 5);
     var latestQueryId;
-		function _setResults(queryId, prop, data) {
-			if (queryId === latestQueryId) {
-				$scope[prop] = data;
-			}
-		}
-		function _fetchData() {
-			var query = $scope.query.trim();
-			if (!query) { return; }
-			var queryId = Number(new Date());
-			latestQueryId = queryId;
-			$http.get(HOST + 'sana/' + query)
-				.success(function(data) {
-					_setResults(queryId, 'sanat', data);
-				});
-			$http.get(HOST + 'full/' + query)
-				.success(function(data) {
-				  _setResults(queryId, 'quotes', data);
-				});
-			logQuery(query);
-		}
-		$scope.update = window.debounce(_fetchData, DEBOUNCE_WAIT);
+    function _setResults(queryId, prop, data) {
+      if (queryId === latestQueryId) {
+        $scope[prop] = data;
+      }
+    }
+    function _fetchData() {
+      var query = $scope.query.trim();
+      if (!query) { return; }
+      var queryId = Number(new Date());
+      latestQueryId = queryId;
+      $http.get(HOST + 'sana/' + query)
+        .success(function(data) {
+          _setResults(queryId, 'sanat', data);
+        });
+      $http.get(HOST + 'full/' + query)
+        .success(function(data) {
+          _setResults(queryId, 'quotes', data);
+        });
+      logQuery(query);
+    }
+    $scope.update = window.debounce(_fetchData, DEBOUNCE_WAIT);
   }]);
 
