51c115a8d8af0b0da65faadc321b4d3b8b818e44
David Rosson <david@rosson.com.au>
2017-08-21 18:17:49 -0700

    Remove caching from models

--- server/models.js
+++ server/models.js
@@ -10,58 +10,34 @@ var DB_URI = 'mongodb://' + DB_PREFIX + DB_HOST + ':' + DB_PORT + '/' + DB_NAME;
 
 var db;
 
-var CACHE_TIMEOUT = 30 * 1000;
-
-var clearCachedItem = function(cache, query) {
-  var timer = setTimeout(function() {
-    delete cache[query];
-    clearTimeout(timer);
-  }, CACHE_TIMEOUT);
-};
-
 mongo.connect(DB_URI, function (err, pool) {
   db = pool;
 });
-
-var wordCache = {},
-    textCache = {};
 
 var models = {};
 
 models.getHeadword = function (query, callback) {
   query = query.toString();
-  if (wordCache[query]) {
-    callback(wordCache[query]);
-  } else {
-    db.collection("sanat").find(
-      { $text: { $search: query } },  
-      { limit: 10, sort: "headword" }, 
-      function (err, cursor) {
-        cursor.toArray(function (err, docs) {
-          callback(docs);
-          wordCache[query] = docs;
-          clearCachedItem(wordCache, query);
-        });
+  db.collection("sanat").find(
+    { $text: { $search: query } },
+    { limit: 10, sort: "headword" },
+    function (err, cursor) {
+      cursor.toArray(function (err, docs) {
+        callback(docs);
       });
-  }
+  });
 };
 
 models.getCitations = function (query, callback) {
   query = query.toString();
-  if (textCache[query]) {
-    callback(textCache[query]);
-  } else {
-    db.collection("citations").find(
-      { $text: { $search: query } },  
-      { limit : 10 }, 
-      function (err, cursor) {
-        cursor.toArray(function (err, docs) {
-          callback(docs);
-          textCache[query] = docs;
-          clearCachedItem(textCache, query);
-        });
+  db.collection("citations").find(
+    { $text: { $search: query } },
+    { limit : 10 },
+    function (err, cursor) {
+      cursor.toArray(function (err, docs) {
+        callback(docs);
       });
-  }
+  });
 };
 
 module.exports = models;
