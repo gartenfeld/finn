6b26df3961c366859fee7aecf48c538528a41b30
David Rosson <david@rosson.com.au>
2017-08-21 21:48:22 -0700

    Tidy up server.js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var api = require('./api');
 
 var app = express();
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.set('port', port); 
 app.set('ipaddr', ip);
