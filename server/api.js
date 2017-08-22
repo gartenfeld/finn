cac2bedf5cec963de932f386e1bbc50a8778294e
David Rosson <david@rosson.com.au>
2017-08-21 21:49:31 -0700

    Fix devastating fat arrow

--- server/api.js
+++ server/api.js
@@ -3,7 +3,7 @@ var models = require('./models');
 function getData(model) {
   return function(req, res) {
     var query = String(req.params.query);
-    return models[model](query).then(docs => {
+    return models[model](query).then(function(docs) {
       return res.status(200).json(docs);
     });
   };
